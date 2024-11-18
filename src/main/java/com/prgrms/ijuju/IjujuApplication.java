3d72d2bdc4dd21e62111bb19b2f802628ab327a4
deveunhwa <deveunhwa@gmail.com>
2024-11-18 11:03:48 +0900

    Chore: 프로젝트 기초 설계

---
+++ src/main/java/com/prgrms/ijuju/IjujuApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.ijuju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjujuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IjujuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgrms/ijuju/IjujuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.ijuju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjujuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
