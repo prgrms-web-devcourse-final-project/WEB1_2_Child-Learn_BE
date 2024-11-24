ec9b87da7522f45c51c7ca627870b744d3e731a1
deveunhwa <deveunhwa@gmail.com>
2024-11-25 05:36:26 +0900

    Feat: Point Repository 구현

---
+++ src/main/java/com/prgrms/ijuju/domain/point/repository/ExchangeRepository.java
@@ -0,0 +1,16 @@
+package com.prgrms.ijuju.domain.point.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.prgrms.ijuju.domain.point.entity.ExchangeDetails;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ExchangeRepository extends JpaRepository<ExchangeDetails, Long> {
+    List<ExchangeDetails> ExchangeHistory(Long memberId, LocalDateTime startDate, LocalDateTime endDate);
+
+    List<ExchangeDetails> findByMemberId(Long memberId);
+}
