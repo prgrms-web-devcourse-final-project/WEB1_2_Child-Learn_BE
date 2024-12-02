d6da82416cea7bb9e499b68e32764428d2a8d598
Preta3418 <jason123qwe@gmail.com>
2024-12-02 12:34:22 +0900

    Refactor: 충돌 수정
    WebSocketConfig 충돌 수정입니다
    
    related to: #51

--- src/main/java/com/prgrms/ijuju/global/config/WebSocketConfig.java
+++ src/main/java/com/prgrms/ijuju/global/config/WebSocketConfig.java
@@ -2,15 +2,9 @@ package com.prgrms.ijuju.global.config;
 
 import com.prgrms.ijuju.domain.stock.adv.advancedinvest.handler.AdvancedInvestWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< Updated upstream
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-@Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-=======
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 import org.springframework.lang.NonNull;
@@ -19,19 +13,12 @@ import org.springframework.lang.NonNull;
 @EnableWebSocketMessageBroker
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSocketConfigurer {
->>>>>>> Stashed changes
+
 
     private final AdvancedInvestWebSocketHandler advancedInvestWebSocketHandler;
 
     public WebSocketConfig(AdvancedInvestWebSocketHandler advancedInvestWebSocketHandler) {
         this.advancedInvestWebSocketHandler = advancedInvestWebSocketHandler;
-    }
-
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(advancedInvestWebSocketHandler, "/api/v1/advanced-invest")
-                .setAllowedOrigins("*") // 모든 출처 허용
-                .withSockJS(); // SockJS 지원
     }
 
 
