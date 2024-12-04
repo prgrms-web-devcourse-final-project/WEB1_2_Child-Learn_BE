c7908da317ef44f45bb29bfc76da34e93defb179
chanspar <qhrn12345@naver.com>
2024-12-04 11:47:30 +0900

    Fix: FCM 환경설정 재설정

--- src/main/java/com/prgrms/ijuju/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/prgrms/ijuju/domain/notification/repository/NotificationRepository.java
@@ -1,4 +1,7 @@
 package com.prgrms.ijuju.domain.notification.repository;
 
-public interface NotificationRepository {
+import com.prgrms.ijuju.domain.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 }
