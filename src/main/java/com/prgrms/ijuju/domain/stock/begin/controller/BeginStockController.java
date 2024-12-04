eb8888dbc7cbdad974fe3e2d354405602f1697f8
1mjay <sumthq@gmail.com>
2024-12-04 12:32:09 +0900

    Feat: 개발 환경에서 모의 투자 초급 데이터 수동 생성 기능 구현
    배포 환경에서 자정에 생성되는 데이터를 수동으로 생성하도록 설정

--- src/main/java/com/prgrms/ijuju/domain/stock/begin/controller/BeginStockController.java
+++ src/main/java/com/prgrms/ijuju/domain/stock/begin/controller/BeginStockController.java
@@ -1,10 +1,14 @@
 package com.prgrms.ijuju.domain.stock.begin.controller;
 
 import com.prgrms.ijuju.domain.stock.begin.dto.response.BeginStockResponse;
+import com.prgrms.ijuju.domain.stock.begin.service.BeginStockGptService;
+import com.prgrms.ijuju.domain.stock.begin.service.BeginStockPriceService;
 import com.prgrms.ijuju.domain.stock.begin.service.BeginStockService;
 import com.prgrms.ijuju.global.auth.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class BeginStockController {
     private final BeginStockService beginStockService;
+    private final BeginStockPriceService beginStockPriceService;
+    private final BeginStockGptService beginStockGptService;
 
     @GetMapping
     public ResponseEntity<BeginStockResponse> getBeginStock() {
@@ -29,5 +35,21 @@ public class BeginStockController {
         return ResponseEntity.ok().build();
     }
 
+    @Profile("dev")
+    @PostMapping("/generations/manual")
+    public ResponseEntity<String> generateDailyData() {
+        log.info("오늘의 모의투자 초급 데이터 수동 생성");
+        try {
+            beginStockPriceService.createWeeklyStockPrice();
+
+            for (int i = 0; i < 5; i++) {
+                beginStockGptService.generateBeginQuiz();
+            }
+            return ResponseEntity.ok("Daily data generated successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to generate data: " + e.getMessage());
+        }
+    }
 
 }
