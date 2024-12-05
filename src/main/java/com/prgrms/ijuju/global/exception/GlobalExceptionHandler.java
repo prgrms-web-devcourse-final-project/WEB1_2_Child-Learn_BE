796647e8cda9a30226437582b13fe29f7324a7f2
1mjay <sumthq@gmail.com>
2024-12-05 09:26:23 +0900

    Feat: 공통 에러 처리 로직 추가 및 도메인별 예외 구조 통합

--- src/main/java/com/prgrms/ijuju/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prgrms/ijuju/global/exception/GlobalExceptionHandler.java
@@ -3,12 +3,10 @@ package com.prgrms.ijuju.global.exception;
 import com.prgrms.ijuju.domain.minigame.flipcard.dto.response.ErrorFlipCardResponse;
 import com.prgrms.ijuju.domain.minigame.flipcard.exception.FlipCardErrorCode;
 import com.prgrms.ijuju.domain.minigame.flipcard.exception.FlipCardException;
-import com.prgrms.ijuju.domain.minigame.wordquiz.dto.response.ErrorWordQuizResponse;
-import com.prgrms.ijuju.domain.minigame.wordquiz.exception.WordQuizErrorCode;
-import com.prgrms.ijuju.domain.minigame.wordquiz.exception.WordQuizException;
 import com.prgrms.ijuju.domain.stock.mid.dto.response.ErrorMidResponse;
 import com.prgrms.ijuju.domain.stock.mid.exception.MidStockErrorCode;
 import com.prgrms.ijuju.domain.stock.mid.exception.MidStockException;
+import com.prgrms.ijuju.global.common.dto.CommonErrorResponse;
 import com.prgrms.ijuju.global.common.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,8 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
@@ -49,11 +47,11 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(errorCode.getStatus()).body(response);
     }
 
-    @ExceptionHandler(WordQuizException.class)
-    protected ResponseEntity<ErrorWordQuizResponse> handleWordQuizException(final WordQuizException e) {
-        log.error("WordQuizException: {}", e.getMessage());
-        final WordQuizErrorCode errorCode = e.getErrorCode();
-        final ErrorWordQuizResponse response = ErrorWordQuizResponse.of(errorCode);
-        return ResponseEntity.status(response.getStatus()).body(response);
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<CommonErrorResponse> handleBusinessException(final BusinessException e) {
+        log.error("{}: {}", e.getClass().getSimpleName(), e.getMessage());
+        final ErrorCode errorCode = e.getErrorCode();
+        final CommonErrorResponse response = CommonErrorResponse.of(errorCode);
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
     }
 }
