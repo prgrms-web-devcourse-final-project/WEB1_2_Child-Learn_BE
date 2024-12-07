aa74c9fadb5ee2ab8914e74a9a34828d8188c8a0
Preta3418 <jason123qwe@gmail.com>
2024-12-07 19:09:22 +0900

    fix: interceptor 에서 '+' 를 string 으로 받는 문제 해결

--- src/main/java/com/prgrms/ijuju/global/auth/JwtHandshakeInterceptor.java
+++ src/main/java/com/prgrms/ijuju/global/auth/JwtHandshakeInterceptor.java
@@ -27,6 +27,8 @@ public class JwtHandshakeInterceptor implements HandshakeInterceptor {
             String query = request.getURI().getQuery();
             if (query != null) {
                 // 모든 쿼리 파라미터를 Map으로 변환
+                query = query.replace("+", " ");
+
                 Map<String, String> queryParams = Arrays.stream(query.split("&"))
                         .map(param -> param.split("=", 2)) // '='로 나누되, 최대 2개만 분리
                         .filter(pair -> pair.length == 2) // 유효한 key-value 쌍만 유지
