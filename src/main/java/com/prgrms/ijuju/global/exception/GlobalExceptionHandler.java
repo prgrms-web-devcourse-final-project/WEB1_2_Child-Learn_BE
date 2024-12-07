1174a747b22b1f8db77444976852870f87b3c7fb
chanspar <qhrn12345@naver.com>
2024-12-07 22:13:13 +0900

    Refactor: 카드뒤집기 글로벌 예외 처리

--- src/main/java/com/prgrms/ijuju/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prgrms/ijuju/global/exception/GlobalExceptionHandler.java
@@ -1,8 +1,5 @@
 package com.prgrms.ijuju.global.exception;
 
-import com.prgrms.ijuju.domain.minigame.flipcard.dto.response.ErrorFlipCardResponse;
-import com.prgrms.ijuju.domain.minigame.flipcard.exception.FlipCardErrorCode;
-import com.prgrms.ijuju.domain.minigame.flipcard.exception.FlipCardException;
 import com.prgrms.ijuju.global.common.dto.ErrorResponse;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,14 +11,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(FlipCardException.class)
-    protected ResponseEntity<ErrorFlipCardResponse> handleFlipCardException(final FlipCardException e) {
-        log.error("FlipCardException: {}", e.getMessage());
-        final FlipCardErrorCode errorCode = e.getErrorCode();
-        final ErrorFlipCardResponse response = ErrorFlipCardResponse.of(errorCode);
-        return ResponseEntity.status(errorCode.getStatus()).body(response);
-    }
 
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
