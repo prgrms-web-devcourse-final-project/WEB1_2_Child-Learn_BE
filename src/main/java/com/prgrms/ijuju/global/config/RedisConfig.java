fae9ec4488eba8cc82bd6033da67edca0d57d9b8
deveunhwa <deveunhwa@gmail.com>
2024-12-09 13:17:41 +0900

    Modify: Chat redis 설정 추가

--- src/main/java/com/prgrms/ijuju/global/config/RedisConfig.java
+++ src/main/java/com/prgrms/ijuju/global/config/RedisConfig.java
@@ -1,14 +1,18 @@
 package com.prgrms.ijuju.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Slf4j
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
@@ -21,7 +25,9 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
+        testRedisConnection(factory);
+        return factory;
     }
     
     @Bean
@@ -31,5 +37,16 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
         return template;
+    }
+
+    private void testRedisConnection(RedisConnectionFactory factory) {
+        try {
+            RedisConnection connection = factory.getConnection();
+            connection.ping();
+            log.info("Redis 서버 연결 성공");
+        } catch (Exception e) {
+            log.error("Redis 서버 연결 실패: {}", e.getMessage());
+            // 연결 실패 시에도 애플리케이션이 시작될 수 있도록 예외를 던지지 않음
+        }
     }
 }
