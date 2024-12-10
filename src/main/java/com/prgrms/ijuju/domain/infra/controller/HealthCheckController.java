21286cd257cb1628ecc46d1636b53c5b0c15233a
1mjay <sumthq@gmail.com>
2024-12-10 10:20:12 +0900

    Add: infra 헬스체크 로깅 추가

--- src/main/java/com/prgrms/ijuju/domain/infra/controller/HealthCheckController.java
+++ src/main/java/com/prgrms/ijuju/domain/infra/controller/HealthCheckController.java
@@ -1,13 +1,22 @@
 package com.prgrms.ijuju.domain.infra.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @RestController
 public class HealthCheckController {
     @GetMapping("/api/v1/healthcheck")
     public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("OK");
+        log.info("Health check requested at {}", LocalDateTime.now());
+        try {
+            return ResponseEntity.ok("OK");
+        } finally {
+            log.info("Health check responded at {}", LocalDateTime.now());
+        }
     }
 }
